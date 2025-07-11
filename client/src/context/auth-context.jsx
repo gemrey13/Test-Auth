4d1fc9a88d13e549bcd9fd0ebe6f29d6a5bdfb2f
gemrey13 <gemreyranola@gmail.com>
2025-07-12 05:54:36 +0800

    fixed

--- client/src/context/auth-context.jsx
+++ client/src/context/auth-context.jsx
@@ -19,7 +19,7 @@ const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await api.post("/auth/token", { email, password });
+      const response = await api.post("/auth/login", { email, password });
       const { access_token, refresh_token } = response.data;
       localStorage.setItem("access_token", access_token);
       localStorage.setItem("refresh_token", refresh_token);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import { router } from "@/routes";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import AuthProvider from "@/context/auth-context";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 );
